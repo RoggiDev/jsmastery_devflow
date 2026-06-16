b9b8eedc14362a228f1119d946864547eb074c6d
RoggiDev <yael.rodriguez.g@outlook.es>
2026-06-15 23:20:56 -0600

    Tailwind CSS Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl">Welcome to the Ultimate Next.js course!</h1>
+      <h1 className="text-light-500 text-3xl font-black">Welcome to the Ultimate Next.js course!</h1>
     </div>
   );
 };

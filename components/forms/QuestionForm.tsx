d7cf7012b9926ccb9c5dc950949da8a050e2d66a
RoggiDev <yael.rodriguez.g@outlook.es>
2026-07-08 22:50:25 -0600

    Multiple Tags Input

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -9,6 +9,8 @@ import { Button } from "../ui/button";
 import { useRef } from "react";
 import { MDXEditorMethods } from "@mdxeditor/editor";
 import dynamic from "next/dynamic";
+import z, { trim } from "zod";
+import TagCard from "../cards/TagCard";
 
 const Editor = dynamic(() => import("@/components/editor"), {
   ssr: false,
@@ -17,7 +19,7 @@ const Editor = dynamic(() => import("@/components/editor"), {
 const QuestionForm = () => {
   const editorRef = useRef<MDXEditorMethods>(null);
 
-  const form = useForm({
+  const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
       title: "",
@@ -26,7 +28,49 @@ const QuestionForm = () => {
     },
   });
 
-  const handleCreateQuestion = () => {};
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: { value: string[] },
+  ) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+
+      const tagInput = e.currentTarget.value.trim();
+
+      if (tagInput && tagInput.length < 15 && !field.value.includes(tagInput)) {
+        form.setValue("tags", [...field.value, tagInput]);
+        e.currentTarget.value = "";
+        form.clearErrors("tags");
+      } else if (tagInput.length > 15) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag should be less than 15 characters",
+        });
+      } else if (field.value.includes(tagInput)) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag already exists",
+        });
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: { value: string[] }) => {
+    const newTags = field.value.filter((t) => t !== tag);
+
+    form.setValue("tags", newTags);
+
+    if (newTags.length === 0) {
+      form.setError("tags", {
+        type: "manual",
+        message: "Tags are required",
+      });
+    }
+  };
+
+  const handleCreateQuestion = (data: z.infer<typeof AskQuestionSchema>) => {
+    console.log(data);
+  };
 
   return (
     <form
@@ -75,7 +119,10 @@ const QuestionForm = () => {
             data-invalid={fieldState.invalid}
             className="flex w-full flex-col"
           >
-            <FieldLabel className="paragraph-semibold text-dark400_light800">
+            <FieldLabel
+              htmlFor={field.name}
+              className="paragraph-semibold text-dark400_light800"
+            >
               Detailed explanation of your problem{" "}
               <span className="text-primary-500">*</span>
             </FieldLabel>
@@ -116,11 +163,25 @@ const QuestionForm = () => {
             <div>
               <Input
                 className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-14 border"
-                {...field}
                 aria-invalid={fieldState.invalid}
                 placeholder="Add tags..."
+                onKeyDown={(e) => handleInputKeyDown(e, field)}
               />
-              Tags
+              {field.value.length > 0 && (
+                <div className="flex-start mt-2.5 flex-wrap gap-2.5">
+                  {field?.value?.map((tag: string) => (
+                    <TagCard
+                      key={tag}
+                      _id={tag}
+                      name={tag}
+                      compact
+                      remove
+                      isButton
+                      handleRemove={() => handleTagRemove(tag, field)}
+                    />
+                  ))}
+                </div>
+              )}
             </div>
 
             <FieldDescription className="body-regular text-light-500 mt-2.5">
@@ -138,7 +199,7 @@ const QuestionForm = () => {
       <div className="mt-16 flex justify-end">
         <Button
           type="submit"
-          className="primary-gradient !text-light-900 w-fit"
+          className="primary-gradient text-light-900! w-fit"
         >
           Ask A Question
         </Button>
--- lib/validations.ts
+++ lib/validations.ts
@@ -52,20 +52,19 @@ export const SignUpSchema = z.object({
 export const AskQuestionSchema = z.object({
   title: z
     .string()
-    .min(5, {
-      message: "Title must be at least 5 characters.",
-    })
-    .max(130, { message: "Title musn't be longer then 130 characters." }),
-  content: z.string().min(100, { message: "Minimum of 100 characters." }),
+    .min(5, { message: "Title is required." })
+    .max(100, { message: "Title cannot exceed 100 characters." }),
+
+  content: z.string().min(1, { message: "Body is required." }),
   tags: z
     .array(
       z
         .string()
-        .min(1, { message: "Tag must have at least 1 character." })
-        .max(15, { message: "Tag must not exceed 15 characters." }),
+        .min(1, { message: "Tag is required." })
+        .max(30, { message: "Tag cannot exceed 30 characters." }),
     )
-    .min(1, { message: "Add at least one tag." })
-    .max(3, { message: "Maximum of 3 tags." }),
+    .min(1, { message: "At least one tag is required." })
+    .max(3, { message: "Cannot add more than 3 tags." }),
 });
 
 export const UserSchema = z.object({

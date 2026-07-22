f23a0b298b68d0c4aadc23c1a7e0bf5da5a659d5
RoggiDev <yael.rodriguez.g@outlook.es>
2026-07-22 00:18:06 -0600

    Account by Provider & API Fetch Handler

--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -1,29 +1,31 @@
 import { NextResponse } from "next/server";
 
-import User from "@/database/user.model";
+import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
-import { UserSchema } from "@/lib/validations";
+import { AccountSchema } from "@/lib/validations";
 
 export async function POST(request: Request) {
-  const { email } = await request.json();
+  const { providerAccountId } = await request.json();
 
   try {
     await dbConnect();
 
-    const validatedData = UserSchema.partial().safeParse({ email });
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
 
     if (!validatedData.success)
       throw ValidationError.fromZodError(validatedData.error);
 
-    const user = await User.findOne({ email });
-    if (!user) throw new NotFoundError("User");
+    const account = await Account.findOne({ providerAccountId });
+    if (!account) throw new NotFoundError("Account");
 
     return NextResponse.json(
       {
         success: true,
-        data: user,
+        data: account,
       },
       { status: 200 },
     );

6495dbb171298f70dcc4965400a6cd3454c8a24f
RoggiDev <yael.rodriguez.g@outlook.es>
2026-07-24 22:25:40 -0600

    Debugging oAuth

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,7 @@ import { IAccount } from "@/database/account.model";
 import { IUser } from "@/database/user.model";
 
 import { fetchHandler } from "./handlers/fetch";
+import ROUTES from "@/constants/routes";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
@@ -13,7 +14,7 @@ export const api = {
       provider,
       providerAccountId,
     }: SignInWithOAuthParams) =>
-      fetchHandler(`${API_BASE_URL}/auth/sign-in-with-oauth`, {
+      fetchHandler(`${API_BASE_URL}/auth/${ROUTES.SIGN_IN_WITH_OAUTH}`, {
         method: "POST",
         body: JSON.stringify({ user, provider, providerAccountId }),
       }),

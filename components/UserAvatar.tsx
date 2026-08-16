2303d9df8b29941b8094591f079a6c8b96295d4c
RoggiDev <yael.rodriguez.g@outlook.es>
2026-08-15 23:59:27 -0600

    Create Question Details UI

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -4,15 +4,23 @@ import Link from "next/link";
 import ROUTES from "@/constants/routes";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { cn } from "@/lib/utils";
 
 interface Props {
   id: string;
   name: string;
   imageUrl?: string | null;
   className?: string;
+  fallbackClassName?: string;
 }
 
-const UserAvatar = ({ id, name, imageUrl, className = "h-9 w-9" }: Props) => {
+const UserAvatar = ({
+  id,
+  name,
+  imageUrl,
+  className = "h-9 w-9",
+  fallbackClassName,
+}: Props) => {
   const initials = name
     .split(" ")
     .map((word: string) => word[0])
@@ -27,14 +35,19 @@ const UserAvatar = ({ id, name, imageUrl, className = "h-9 w-9" }: Props) => {
           <Image
             src={imageUrl ?? ""}
             alt={name}
+            className="object-cover"
             width={36}
             height={36}
             quality={100}
-            className="object-cover"
           />
         </AvatarImage>
 
-        <AvatarFallback className="primary-gradient font-space-grotesk font-bold tracking-wider text-white">
+        <AvatarFallback
+          className={cn(
+            "primary-gradient font-space-grotesk font-bold tracking-wider text-white",
+            fallbackClassName,
+          )}
+        >
           {initials}
         </AvatarFallback>
       </Avatar>

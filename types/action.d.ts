797d2981f664b90ee1994f22271124845df5d149
RoggiDev <yael.rodriguez.g@outlook.es>
2026-09-09 23:11:59 -0600

    Implement User Questions Tab

--- types/action.d.ts
+++ types/action.d.ts
@@ -71,3 +71,10 @@ interface CollectionBaseParams {
 interface GetUserParams {
   userId: string;
 }
+
+interface GetUserQuestionsParams extends Omit<
+  PaginatedSearchParams,
+  "query" | "filter" | "sort"
+> {
+  userId: string;
+}

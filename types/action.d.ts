4f913ee17ecd2896e0fb10f2c5562935a4d210f4
RoggiDev <yael.rodriguez.g@outlook.es>
2026-09-11 23:41:21 -0600

    Implement User Tags

--- types/action.d.ts
+++ types/action.d.ts
@@ -82,3 +82,7 @@ interface GetUserQuestionsParams extends Omit<
 interface GetUserAnswersParams extends PaginatedSearchParams {
   userId: string;
 }
+
+interface GetUserTagsParams {
+  userId: string;
+}
